f919413e9bfeaa7e946bb426e5f2edf796b59ff1
tazrian-amin <amin.tazrian1979@gmail.com>
2023-11-24 12:49:57 +0600

    added user interface and other setup

---
+++ src/app/modules/user/user.interface.ts
@@ -0,0 +1,25 @@
+export type Product = {
+  productName: string;
+  price: number;
+  quantity: number;
+};
+
+export type User = {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: Product[];
+};
